2da3d7b86a6243e618c82b8708902e98c828becd
Graham Cockell <graham.cockell@outlook.com>
2021-10-16 15:07:56 +0100

    ESP32Duino pin definitions

--- boards/ESP32_ESP32DUINO/pins_ESP32DUINO.hpp
+++ boards/ESP32_ESP32DUINO/pins_ESP32DUINO.hpp
@@ -4,126 +4,100 @@
 
 #pragma once
 
-// DRIVER_TYPE_ULN2003 requires 4 digital outputs in Arduino pin numbering
-#ifndef RA_IN1_PIN
-    #define RA_IN1_PIN 13
-#endif
-#ifndef RA_IN2_PIN
-    #define RA_IN2_PIN 12
-#endif
-#ifndef RA_IN3_PIN
-    #define RA_IN3_PIN 14
-#endif
-#ifndef RA_IN4_PIN
-    #define RA_IN4_PIN 27
-#endif
-#ifndef DEC_IN1_PIN
-    #define DEC_IN1_PIN 26
-#endif
-#ifndef DEC_IN2_PIN
-    #define DEC_IN2_PIN 25
-#endif
-#ifndef DEC_IN3_PIN
-    #define DEC_IN3_PIN 33
-#endif
-#ifndef DEC_IN4_PIN
-    #define DEC_IN4_PIN 32
-#endif
+
 // DRIVER_TYPE_TMC2209_UART requires 4 digital pins in Arduino pin numbering
 #ifndef RA_STEP_PIN
-    #define RA_STEP_PIN 19  // STEP
+    #define RA_STEP_PIN 26  // STEP
 #endif
 #ifndef RA_DIR_PIN
-    #define RA_DIR_PIN 21  // DIR
+    #define RA_DIR_PIN 16  // DIR
 #endif
 #ifndef RA_EN_PIN
-    #define RA_EN_PIN 22  // Enable
+    #define RA_EN_PIN 12  // Enable
 #endif
 #ifndef RA_DIAG_PIN
-    #define RA_DIAG_PIN 23  // only needed for autohome function
+    #define RA_DIAG_PIN -1  // only needed for autohome function
 #endif
 #ifndef RA_MS0_PIN
-    #define RA_MS0_PIN 4
+    #define RA_MS0_PIN -1
 #endif
 #ifndef RA_MS1_PIN
-    #define RA_MS1_PIN 0
+    #define RA_MS1_PIN -1
 #endif
 #ifndef RA_MS2_PIN
-    #define RA_MS2_PIN 2
+    #define RA_MS2_PIN -1
 #endif
-// DRIVER_TYPE_TMC2209_UART HardwareSerial port, can be shared across all drivers
-#ifndef RA_SERIAL_PORT
-    #define RA_SERIAL_PORT Serial2
+// DRIVER_TYPE_TMC2209_UART requires 2 additional digital pins for SoftwareSerial, can be shared across all drivers
+#ifndef RA_SERIAL_PORT_TX
+    #define RA_SERIAL_PORT_TX 4  // SoftwareSerial TX port
+#endif
+#ifndef RA_SERIAL_PORT_RX
+    #define RA_SERIAL_PORT_RX 49  // SoftwareSerial RX port
 #endif
 #ifndef RA_DRIVER_ADDRESS
     #define RA_DRIVER_ADDRESS 0b00  // Set by MS1/MS2. LOW/LOW in this case
 #endif
 // DRIVER_TYPE_TMC2209_UART requires 4 digital pins in Arduino pin numbering
 #ifndef DEC_STEP_PIN
-    #define DEC_STEP_PIN 16  // STEP
+    #define DEC_STEP_PIN 25  // STEP
 #endif
 #ifndef DEC_DIR_PIN
-    #define DEC_DIR_PIN 17  // DIR
+    #define DEC_DIR_PIN 27  // DIR
 #endif
 #ifndef DEC_EN_PIN
-    #define DEC_EN_PIN 5  // Enable
+    #define DEC_EN_PIN 12  // Enable
 #endif
 #ifndef DEC_DIAG_PIN
-    #define DEC_DIAG_PIN 18  // only needed for autohome function
+    #define DEC_DIAG_PIN -1  // only needed for autohome function
 #endif
 #ifndef DEC_MS0_PIN
-    #define DEC_MS0_PIN 15
+    #define DEC_MS0_PIN -1
 #endif
 #ifndef DEC_MS1_PIN
-    #define DEC_MS1_PIN 8
+    #define DEC_MS1_PIN -1
 #endif
 #ifndef DEC_MS2_PIN
-    #define DEC_MS2_PIN 7
+    #define DEC_MS2_PIN -1
 #endif
-// DRIVER_TYPE_TMC2209_UART HardwareSerial port, can be shared across all drivers
-#ifndef DEC_SERIAL_PORT
-    #define DEC_SERIAL_PORT Serial2  // SoftwareSerial TX port
+// DRIVER_TYPE_TMC2209_UART requires 2 additional digital pins for SoftwareSerial, can be shared across all drivers
+#ifndef DEC_SERIAL_PORT_TX
+    #define DEC_SERIAL_PORT_TX 4  // SoftwareSerial TX port
+#endif
+#ifndef DEC_SERIAL_PORT_RX
+    #define DEC_SERIAL_PORT_RX 49  // SoftwareSerial RX port
 #endif
 #ifndef DEC_DRIVER_ADDRESS
     #define DEC_DRIVER_ADDRESS 0b01  // Set by MS1/MS2 (MS1 HIGH, MS2 LOW)
 #endif
 
-#define SW_SERIAL_UART 0
+#define SW_SERIAL_UART 1
+#ifndef UART_CONNECTION_TEST_TXRX
+    #define UART_CONNECTION_TEST_TXRX 1
+#endif
 
-// DRIVER_TYPE_TMC2209_UART requires 4 digital pins in Arduino pin numbering
-#ifndef AZ_STEP_PIN
-    #define AZ_STEP_PIN 46  // STEP
-#endif
-#ifndef AZ_DIR_PIN
-    #define AZ_DIR_PIN 48  // DIR
-#endif
-#ifndef AZ_EN_PIN
-    #define AZ_EN_PIN 62  // Enable
-#endif
-#ifndef AZ_DIAG_PIN
-    #define AZ_DIAG_PIN 18  // only needed for autohome function
-#endif
 // DRIVER_TYPE_TMC2209_UART requires 4 digital pins in Arduino pin numbering
 #ifndef ALT_STEP_PIN
-    #define ALT_STEP_PIN 26  // STEP
+    #define ALT_STEP_PIN 17  // STEP
 #endif
 #ifndef ALT_DIR_PIN
-    #define ALT_DIR_PIN 28  // DIR
+    #define ALT_DIR_PIN 14  // DIR
 #endif
 #ifndef ALT_EN_PIN
-    #define ALT_EN_PIN 24  // Enable
+    #define ALT_EN_PIN 12  // Enable
 #endif
 #ifndef ALT_DIAG_PIN
-    #define ALT_DIAG_PIN 2  // only needed for autohome function
+    #define ALT_DIAG_PIN -1  // only needed for autohome function
 #endif
-
-// DISPLAY_TYPE_LCD_JOY_I2C_SSD1306 requires 3 analog inputs in Arduino pin numbering
-#ifndef LCD_KEY_SENSE_X_PIN
-    #define LCD_KEY_SENSE_X_PIN 34
+// DRIVER_TYPE_TMC2209_UART requires 4 digital pins in Arduino pin numbering
+#ifndef AZ_STEP_PIN
+    #define AZ_STEP_PIN 19  // STEP
 #endif
-#ifndef LCD_KEY_SENSE_Y_PIN
-    #define LCD_KEY_SENSE_Y_PIN 39
+#ifndef AZ_DIR_PIN
+    #define AZ_DIR_PIN 18  // DIR
 #endif
-#ifndef LCD_KEY_SENSE_PUSH_PIN
-    #define LCD_KEY_SENSE_PUSH_PIN 36
+#ifndef AZ_EN_PIN
+    #define AZ_EN_PIN 12  // Enable
+#endif
+#ifndef AZ_DIAG_PIN
+    #define AZ_DIAG_PIN -1  // only needed for autohome function
 #endif
